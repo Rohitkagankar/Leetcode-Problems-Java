43fa70b1e0ae7ba969c1f02ba0b732ba2006c2a9
Rohit Rajaram Kagankar <112383067+Rohitkagankar@users.noreply.github.com>
2024-06-10 12:52:11 +0530

    check num is pallindrone

---
+++ A9_numIsPallindrone.java
@@ -0,0 +1,23 @@
+public class A9_numIsPallindrone {
+    public static boolean isPalindrone(int x){
+        if(x<0){
+            return false;
+        }
+        int original=x;
+        int reversed=0;
+
+        while(x != 0){
+            int digit=x %10;
+            if(reversed>(Integer.MAX_VALUE-digit)/10){
+                return false;
+            }
+            reversed=reversed*10+digit;
+            x/=10;
+        }
+        return original==reversed;
+    }
+    public static void main(String[] args) {
+        A9_numIsPallindrone sol=new A9_numIsPallindrone();
+        System.out.println(sol.isPalindrone(13331));
+    }
+}
