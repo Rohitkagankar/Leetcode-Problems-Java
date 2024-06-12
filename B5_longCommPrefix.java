a64f34282373ff43e88d2cdc2ed586ceadab7d66
Rohit Rajaram Kagankar <112383067+Rohitkagankar@users.noreply.github.com>
2024-06-12 09:28:07 +0530

    logest common substring

---
+++ B5_longCommPrefix.java
@@ -0,0 +1,27 @@
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+public class B5_longCommPrefix {
+    public static String longCommPrefix(String [] strs){
+        Arrays.sort(strs);
+        String str1=strs[0];
+        String str2=strs[strs.length-1];
+        int index=0;
+        while(index<str1.length()){
+            if(str1.charAt(index)==str2.charAt(index)){
+                index++;
+            }else{
+                break;
+            }     
+        }
+        return index==0 ? " " : str1.substring(0, index);
+
+    }
+    public static void main(String[] args) {
+        String[] strs1 = {"flower", "flow", "flight"};
+        System.out.println(longCommPrefix(strs1));
+
+        String[] strs3 = {"interspecies", "interstellar", "interstate"};
+        System.out.println(longCommPrefix(strs3));
+    }
+}
