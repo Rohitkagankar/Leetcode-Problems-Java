f2acd589d5722739a108cca5d23355c091c6fd66
Rohit Rajaram Kagankar <112383067+Rohitkagankar@users.noreply.github.com>
2024-06-19 17:48:57 +0530

    search insert

---
+++ D8_search_insert.java
@@ -0,0 +1,27 @@
+public class D8_search_insert {
+    public static int searchInsert(int [] nums, int target){
+        if(nums==null && nums.length==0) return -1;
+        int left=0;
+        int right=nums.length-1;
+        while (left <= right) {
+            int mid=left+(right-left)/2;
+            if(nums[mid]== target){
+                return mid;
+
+            }else if(nums[mid] > target){
+                right=mid-1;
+            }else{
+                left=mid+1;
+            }
+            
+        }
+        return left;
+    }
+    public static void main(String[] args) {
+        D8_search_insert sol=new D8_search_insert();
+        int[] nums1 = {1, 3, 5, 6};
+        int target1 = 5;
+        System.out.println("Insert position of " + target1 + ": " + sol.searchInsert(nums1, target1));
+        
+    }
+}
