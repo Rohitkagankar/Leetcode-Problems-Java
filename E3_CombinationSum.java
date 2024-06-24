3977c86b873abc6242243d773632f817aaec6b50
Rohit Rajaram Kagankar <112383067+Rohitkagankar@users.noreply.github.com>
2024-06-24 20:23:32 +0530

    combination sum

---
+++ E3_CombinationSum.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+public class E3_CombinationSum {
+    public static List<List<Integer>> CombinationSum (int [] candidates,int target){
+        List<List<Integer>> result=new ArrayList<>();
+
+        backtrack(candidates,target,0,new ArrayList<>(),result);
+        return result;
+    }
+    private static void backtrack(int [] candidates, int target,int start,List<Integer> currentCombination,List<List<Integer>> result){
+        if(target==0){
+            result.add(new ArrayList<>(currentCombination));
+            return;
+        }
+        if(target<0){
+            return;
+        }
+        for(int i=0; i<candidates.length; i++){
+            currentCombination.add(candidates[i]);
+            backtrack(candidates, target-candidates[i], i, currentCombination, result);
+            currentCombination.remove(currentCombination.size()-1);
+        }
+    }
+    public static void main(String[] args) {
+        E3_CombinationSum sol=new E3_CombinationSum();
+        int[] candidates={2,3,6,7};
+        int target=7;
+        List<List<Integer>> combinations=sol.CombinationSum(candidates, target);
+        System.out.println(combinations);
+    }
+}
