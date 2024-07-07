9f3e0e2a344f98d78180d60101b7090b6a79d063
Rohit Rajaram Kagankar <112383067+Rohitkagankar@users.noreply.github.com>
2024-07-08 00:01:10 +0530

    is valid number

--- H2_ValidNumber.java
+++ H2_ValidNumber.java
@@ -1,43 +1,46 @@
 public class H2_ValidNumber {
     public boolean isValid(String s){
-        if(s==null || s.length()==0){
+        if (s == null || s.length() == 0) {
             return false;
         }
-        s.trim();
-        boolean hasNum=false;
-        boolean hasDot=false;
-        boolean hasE=false;
-
-        for(int i=0; i<s.length(); i++){
-
-            char c=s.charAt(i);
-            if(c>='0' && c<='9'){
-                hasNum=true;
-            }else if(c=='.'){
-                if(hasDot || hasE){
+        
+        s = s.trim();
+        
+        boolean hasNum = false;
+        boolean hasDot = false;
+        boolean hasE = false;
+        
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            
+            if (c >= '0' && c <= '9') {
+                hasNum = true;
+            } else if (c == '.') {
+                if (hasDot || hasE) {
                     return false;
                 }
-                hasDot=true;
-            }else if(c=='e' || c=='E'){
-                if(hasE || !hasNum){
+                hasDot = true;
+            } else if (c == 'e' || c == 'E') {
+                if (hasE || !hasNum) {
                     return false;
                 }
-                hasE=true;
-                hasNum=false;
-            }else if(c=='+' || c=='-'){
-                if(i!=0 && s.charAt(i-1) !='e' && s.charAt(i-1) !='E'){
+                hasE = true;
+                hasNum = false;
+            } else if (c == '+' || c == '-') {
+                if (i != 0 && s.charAt(i - 1) != 'e' && s.charAt(i - 1) != 'E') {
                     return false;
                 }
-                hasNum=false;
-            }else{
+                hasNum = false;
+            } else {
                 return false;
             }
         }
+        
         return hasNum;
     }
     public static void main(String[] args) {
         H2_ValidNumber sol=new H2_ValidNumber();
-        String str="0.2";
+        String str=" -90e3   ";
         System.out.println(sol.isValid(str));
     }
 }
