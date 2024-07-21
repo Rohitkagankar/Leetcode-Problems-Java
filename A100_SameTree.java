54b8427247a37a44f8c7c59aa7c1ca203485b85a
Rohit Rajaram Kagankar <112383067+Rohitkagankar@users.noreply.github.com>
2024-07-22 00:52:00 +0530

    same tree

---
+++ A100_SameTree.java
@@ -0,0 +1,38 @@
+
+public class A100_SameTree {
+    public static  class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(int x) { val = x; }
+    }
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null && q == null) {
+            return true;
+        }
+
+        if (p == null || q == null) {
+            return false;
+        }
+
+        if (p.val != q.val) {
+            return false;
+        }
+
+        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+    }
+    public static void main(String[] args) {
+        TreeNode tree1 = new TreeNode(1);
+        tree1.left = new TreeNode(2);
+        tree1.right = new TreeNode(3);
+
+        TreeNode tree2 = new TreeNode(1);
+        tree2.left = new TreeNode(2);
+        tree2.right = new TreeNode(3);
+
+        A100_SameTree solution = new A100_SameTree();
+        boolean result = solution.isSameTree(tree1, tree2);
+        
+        System.out.println("Are the two trees the same? " + result);
+    }
+}
