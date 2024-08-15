ea22a6c7a97a844b3e5613ea8e6e24a8dc89d33e
Rohit Rajaram Kagankar <112383067+Rohitkagankar@users.noreply.github.com>
2024-08-15 23:33:07 +0530

    remove element from ll

---
+++ A203_removeLLelement.java
@@ -0,0 +1,59 @@
+public class A203_removeLLelement {
+    public static  class ListNode {
+        int val;
+        ListNode next;
+        ListNode(int x) { val = x; }
+    }
+
+    public ListNode removeElements(ListNode head, int val) {
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        ListNode current = dummy;
+        
+        while (current.next != null) {
+            if (current.next.val == val) {
+                current.next = current.next.next;
+            } else {
+                current = current.next;
+            }
+        }
+        
+        return dummy.next;
+    }
+
+    public static void printList(ListNode head) {
+        ListNode current = head;
+        while (current != null) {
+            System.out.print(current.val + " ");
+            current = current.next;
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        // Create a linked list: 1 -> 2 -> 6 -> 3 -> 4 -> 5 -> 6
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(6);
+        head.next.next.next = new ListNode(3);
+        head.next.next.next.next = new ListNode(4);
+        head.next.next.next.next.next = new ListNode(5);
+        head.next.next.next.next.next.next = new ListNode(6);
+
+        // Value to remove
+        int val = 6;
+
+        // Print the original list
+        System.out.print("Original list: ");
+        printList(head);
+
+        // Remove the elements
+        A203_removeLLelement solution = new A203_removeLLelement();
+        ListNode modifiedHead = solution.removeElements(head, val);
+
+        // Print the modified list
+        System.out.print("Modified list after removing " + val + ": ");
+        printList(modifiedHead);
+    }
+    
+}
