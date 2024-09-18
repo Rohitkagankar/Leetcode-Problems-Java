020592268f6c255d873a32968a2923c474f8f68f
Rohit Rajaram Kagankar <112383067+Rohitkagankar@users.noreply.github.com>
2024-09-18 23:14:35 +0530

    degree of an array

---
+++ A697_DegreeOfAnArray.java
@@ -0,0 +1,40 @@
+import java.util.HashMap;
+
+public class A697_DegreeOfAnArray {
+    public int findShortestSubArray(int[] nums) {
+        
+        HashMap<Integer, Integer> left = new HashMap<>();
+        HashMap<Integer, Integer> right = new HashMap<>();
+        HashMap<Integer, Integer> count = new HashMap<>();
+        
+        int degree = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            int num = nums[i];
+            
+            left.putIfAbsent(num, i);
+            
+            right.put(num, i);
+            
+            count.put(num, count.getOrDefault(num, 0) + 1);
+            
+            degree = Math.max(degree, count.get(num));
+        }
+        
+        int minLength = nums.length;
+
+        for (int num : count.keySet()) {
+            if (count.get(num) == degree) {
+                int length = right.get(num) - left.get(num) + 1;
+                minLength = Math.min(minLength, length);
+            }
+        }
+        
+        return minLength;
+    }
+    public static void main(String[] args) {
+        A697_DegreeOfAnArray solution = new A697_DegreeOfAnArray();
+        int[] nums = {1, 2, 2, 3, 1};
+        System.out.println("Shortest subarray length: " + solution.findShortestSubArray(nums));  // Output: 2
+    }
+}
